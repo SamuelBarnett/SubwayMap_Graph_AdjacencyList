ea3c996742cb9b4f9c8226210de89867428ef62f
Samuel Barnett <sambiam888@gmail.com>
2023-01-20 10:51:00 -0500

    template filled in

---
+++ LondonGraph/LondonGraph.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LondonGraph
+{
+    public enum Colour { RED, YELLOW, GREEN } // For example
+    public class SubwayMap
+    {
+        // edge
+        private class Node
+        {
+            //edge
+            public Station connection; // Adjacent station (connection)
+            public Colour line; // Colour of its subway line
+            public Node next; // Link to the next adjacent station (Node)
+            public Node() { }
+            public Node(Station connection, Colour c, Node next) { }
+        }
+        // vertex
+        private class Station
+        {
+            public string name; // Name of the subway station
+            public bool visited; // Used for the breadth-first search
+            public Node E; // Header node for a linked list of adjacent stations
+            public Station(string name)
+            {
+
+            }
+        }
+        private Dictionary<string, Station> S; // Dictionary of stations
+        // Adjacency list.
+        public SubwayMap() { }
+        public void InsertStation(string name) { }
+        public bool RemoveStation(string name) { }
+        public bool InsertConnection(string name1, string name2, Colour c) { }
+        public bool RemoveConnection(string name1, string name2, Colour c) { }
+        public void ShortestRoute(string name1, string name2) { }
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Assignment 1 3020
+
+A graph representing a subway system with an adjacency list
